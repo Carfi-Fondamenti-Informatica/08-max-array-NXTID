918f7f54e8447537523eac231399372f826c7ca1
NXTID <117934144+NXTID@users.noreply.github.com>
2022-12-12 09:20:05 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int a=0;
+
+    cin>>a;
+    float insieme[a];
+    for(int i=0;i<a;i++){
+        cin>>insieme[i];
+    }
+    cout<<maxarray(insieme,a);
+
+    return 0;
 }
