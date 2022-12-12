01ac007cf7a0fad68ccb3043c681b1104b1dd9ec
NXTID <117934144+NXTID@users.noreply.github.com>
2022-12-12 09:19:57 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,10 @@
+float maxarray(const float insieme[], int d){
+    float x=0;
 
+    x=insieme[0];
+    for(int i=0;i<d;i++){
+        if(x<insieme[i])
+            x=insieme[i];
+    }
+    return x;
+}
